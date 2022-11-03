4656c65a8f9ec49c93fcc9585bd9ce2950aee94a
Alvaro <Alvaro9943310@gmail.com>
2022-11-03 11:28:46 -0300

    copiado de frontend en bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { FotoDePerfilComponent } from './components/foto-de-perfil/foto-de-perfil.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    BannerComponent,
+    AcercaDeComponent,
+    FotoDePerfilComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/foto-de-perfil/foto-de-perfil.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-foto-de-perfil',
+  templateUrl: './foto-de-perfil.component.html',
+  styleUrls: ['./foto-de-perfil.component.css']
+})
+export class FotoDePerfilComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
