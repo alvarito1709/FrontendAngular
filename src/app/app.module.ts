971efd7c688b802d41e096a69f6f929b1841777e
Alvaro <Alvaro9943310@gmail.com>
2022-11-11 18:15:59 -0300

    se agrego contactoComponent, skillComponent y el routingModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -7,6 +8,15 @@ import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { FotoDePerfilComponent } from './components/foto-de-perfil/foto-de-perfil.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { AppRoutingModule } from './app-routing.module';
+
+
+const appRoutes: Routes = [
+{path: '', component: AppComponent},
+{path: 'contacto', component: ContactoComponent}
+]
 
 @NgModule({
   declarations: [
@@ -15,10 +25,15 @@ import { FooterComponent } from './components/footer/footer.component';
     BannerComponent,
     AcercaDeComponent,
     FotoDePerfilComponent,
-    FooterComponent
+    FooterComponent,
+    ContactoComponent,
+    SkillsComponent,
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes, {enableTracing:true}),
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
